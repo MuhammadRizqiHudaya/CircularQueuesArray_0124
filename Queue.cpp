7efc7e1142ef02ecb7dec2cd67eeb434327099f3
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-12 14:53:46 +0700

    cek apakah antrian kosng

--- Queue.cpp
+++ Queue.cpp
@@ -29,5 +29,21 @@ public:
             return;
         }
 
-        
+        //2.cek apakah antrian kosong
+        if (FRONT == -1) 
+        {
+            FRONT = 0;
+            REAR = 0;
+        } 
+        else 
+        {
+        if (REAR == max -1)
+        REAR=0;
+        else
+        REAR = REAR + 1;
+        } 
+        queue_array[REAR]=num;
+    }
+
+   
 };
