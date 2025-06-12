ef61044c9882645fe76f20a4f07b952c542bb175
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-12 15:29:44 +0700

    int main

--- Queue.cpp
+++ Queue.cpp
@@ -114,7 +114,56 @@ public:
 
 };
 
+int main()
+{
+    Queue q;
+    char ch;
 
+    while (true)
+    {
+        try
+        {
+            cout << "menu" << endl;
+            cout << "1. implement insert operation" << endl;
+            cout << "2. implement delete operation" << endl;
+            cout << "3. display values" << endl;
+            cout << "4. exit" << endl;
+            cout << "enter your choice (1-4) : ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "invalid option!!" <<endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "check for the values entered." << endl;
+        }
         
     }
 }
