7e579668a662544bca986d7c0d0a077217786271
HudayaW <hudayamuhammadrizqi@gmail.com>
2025-06-12 15:26:04 +0700

    jika FRONT_position > rear position, iterasi dari FRONT hingga akhir array

--- Queue.cpp
+++ Queue.cpp
@@ -92,6 +92,29 @@ public:
             }
             cout << endl;
         }
+        else
+        {
+            //jika FRONT_position > rear position, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max -1)
+            {
+                cout << queue_array[FRONT_position] << "";
+                FRONT_position++;
+            }
+
+            FRONT_position = 0;
+
+             while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << "";
+                FRONT_position++;
+            }
+            cout << endl;
+        }
+    }
+
+};
+
+
         
     }
 }
